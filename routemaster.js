d6983e027b3253be4c5119c51e9fe6135b5e5947
Chris Newton <chrisn@brandwatch.com>
2015-08-20 14:23:27 +0100

    wip

--- routemaster.js
+++ routemaster.js
@@ -3,6 +3,7 @@
 var fs = require('fs');
 var path = require('path');
 var Router;
+var logger;
 
 function resolveDirectory(directory){
     var parentDirectory = path.dirname(module.parent.filename);
@@ -10,16 +11,33 @@ function resolveDirectory(directory){
     return path.resolve(parentDirectory, directory);
 }
 
+function warn(message){
+    var message = 'Routemaster:' + message;
+
+    if(logger){
+        logger.warn(message);
+    }else{
+        console.warn(message);
+    }
+}
+
 function getRoutingFiles(directory){
     var target = resolveDirectory(directory);
 
-    return fs.readdirSync(target).reduce(function(files, route){
-        route = path.join(target, route);
+    return fs.readdirSync(target).reduce(function(files, filename){
+        var fullyQualifiedFilename = path.join(target, filename);
 
-        if(fs.lstatSync(route).isDirectory()){
-            return files.concat(getRoutingFiles(route));
+        if(fs.lstatSync(fullyQualifiedFilename).isDirectory()){
+            return files.concat(getRoutingFiles(fullyQualifiedFilename));
         }
-        files.push(route);
+
+        if(filename[0] === '.'){
+            warn('Skipping hidden file "' + fullyQualifiedFilename + '"');
+        }else if(filename.indexOf('.js') == -1){
+            warn('Skipping non-js file "' + fullyQualifiedFilename + '"');
+        }else{
+            files.push(fullyQualifiedFilename);
+        }
 
         return files;
     }, []);
@@ -31,10 +49,14 @@ function buildRouter(masterRouter, routingFile){
 
     try{
         routingFn = require(routingFile);
-    } catch(e){}
+    }catch(e){
+        warn('Skipping unparsable file "' + routingFile + '"');
+    }
 
     if(typeof routingFn === 'function'){
         routingFn(router);
+    }else{
+        warn('Skipping "' + routingFile + '" as it doesn\'t export a function');
     }
 
     masterRouter.use(router);
@@ -52,6 +74,10 @@ module.exports = function routemaster(options){
 
     if(!options.directory){
         throw new Error('Routemaster require a directory option');
+    }
+
+    if(options.logger && typeof options.logger.warn !== 'function'){
+        throw new Error('Routermaster expects a logger with a warn method');
     }
 
     var routingFiles = getRoutingFiles(options.directory);
